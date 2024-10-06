27c74dd94d5687fe8aa9ff0e90beb26262fb69e4
Dylan Shaffer <swimdoctor5@gmail.com>
2024-10-05 23:01:21 -0400

    SetBaseMenus

--- LudumDare56/Assets/Scripts/UI/OOB Menu.cs
+++ LudumDare56/Assets/Scripts/UI/OOB Menu.cs
@@ -6,25 +6,80 @@ using UnityEngine.UI;
 
 public class OOBMenu : MonoBehaviour
 {
-    public GameObject MenuPanel;
-    private bool isMenuActive = false;
-
-    void Start()
+    public enum MenuState
     {
-        GetComponent<Image>().enabled = false;
+        None,
+        Menu,
+        Party,
+        Bag,
+        Settings
     }
 
-    void Update()
+    public MenuState menuState = MenuState.None;
+
+    public GameObject menu;
+    public GameObject party;
+    public GameObject bag;
+
+
+
+	private void Start()
+	{
+        UpdateMenu();
+	}
+
+	void Update()
     {
         if (Input.GetKeyDown(KeyCode.M))
         {
-            ToggleMenu();
-        }
+            if(menuState == MenuState.None)
+                SwapState(1);
+            else
+                SwapState(0);
+		}
     }
 
-    public void ToggleMenu()
+    public void UpdateMenu()
     {
-        isMenuActive = !isMenuActive;
-        GetComponent<Image>().enabled = isMenuActive;
+        switch(menuState)
+        {
+            case MenuState.None:
+                menu.SetActive(false);
+                party.SetActive(false);
+                bag.SetActive(false);
+                //Close other menus
+                break;
+            case MenuState.Menu:
+				menu.SetActive(true);
+				party.SetActive(false);
+				bag.SetActive(false);
+				//Close other menus
+				break;
+            case MenuState.Party:
+				menu.SetActive(true);
+				party.SetActive(true);
+				bag.SetActive(false);
+				//Close other menus
+				break;
+            case MenuState.Bag:
+				menu.SetActive(true);
+				party.SetActive(false);
+				bag.SetActive(true);
+				//Close other menus
+				break;
+            case MenuState.Settings:
+				menu.SetActive(true);
+				party.SetActive(false);
+				bag.SetActive(false);
+				//OpenSettingsMenu
+				//Close other menus
+				break;
+		}
     }
+
+    public void SwapState(int state)
+    {
+        menuState = (MenuState)state;
+        UpdateMenu();
+	}
 }
