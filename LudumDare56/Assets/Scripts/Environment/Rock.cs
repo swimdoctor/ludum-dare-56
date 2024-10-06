2c55089b7150a919163a579e63dba9218ac18b6e
Sean Osman <96084662+Sean-Osman@users.noreply.github.com>
2024-10-06 13:42:37 -0600

    add transition to battle scene from world

--- LudumDare56/Assets/Scripts/Environment/Rock.cs
+++ LudumDare56/Assets/Scripts/Environment/Rock.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 public class Rock : MonoBehaviour, IInteractable
 {
-    
+    public string CombatScene;
 
     public void Interact(GameObject User)
     {
+        SceneManager.LoadScene(CombatScene);
         Debug.Log("Interacted with rock");
     }
 
