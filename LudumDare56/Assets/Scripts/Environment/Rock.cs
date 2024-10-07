cb0332d433d863ba5573a006ccaf0b13bbda7f7c
Dylan Shaffer <swimdoctor5@gmail.com>
2024-10-07 15:16:45 -0400

    Push Merge Finish

--- LudumDare56/Assets/Scripts/Environment/Rock.cs
+++ LudumDare56/Assets/Scripts/Environment/Rock.cs
@@ -11,7 +11,6 @@ public class Rock : MonoBehaviour, IInteractable
     public void Interact(GameObject User)
     {
         SceneManager.LoadScene(CombatScene);
-        Debug.Log("Interacted with wizard tower");
     }
 
 
